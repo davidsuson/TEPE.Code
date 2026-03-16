215c0eb8f839d56cd5c7c43f792e32fa2f68315a
davidsuson <susondavidimpo@gmail.com>
2026-03-17 01:27:40 +1100

    Added additional functionality and client information for link validity checks.

--- R/read_pc_reports.R
+++ R/read_pc_reports.R
@@ -11,33 +11,51 @@ read_pc_reports.data.frame <- function(reports){
       report_name <- report["Name"]
       report_link <- report["Link"]
 
-      pages_suffix <- c(report_page = "/report/", submission_page = "/submissions/")
-      pages <- lapply(pages_suffix, function(page_suffix) paste0(report_link, page_suffix))
-      isvalid <- valid_links(pages)
+      valid_links <- valid_links(report_link)
 
-      if (isvalid){
-        report_released_date <- read_pc_report_details(pages$report_page)
-        submissions <- read_pc_report_submissions(pages$submission_page)
+      if (length(valid_links) != 0){
 
-        final_report_details <- submissions %>%
-          dplyr::mutate(
-            Report_Name = report_name,
-            Report_Link = report_link,
-            Report_Released = report_released_date,
-            Valid = TRUE
-          )
+        final_report_details <- tryCatch(
+          error = function(cnd){
+            cli::cli_alert_danger("{report_name} at {report_link} failed to process.")
 
-        cli::cli_alert_success("{report_name} at {report_link} processed succesfully.")
+            final_report_details <- data.frame(
+              Report_Name = report_name,
+              Report_Link = report_link,
+              Unknown_Error = TRUE
+            )
+
+          },
+          {
+
+            report_released_date <- read_pc_report_details(valid_links$report_page)
+            submissions <- read_pc_report_submissions(valid_links$submission_page)
+
+            final_report_details <- submissions %>%
+              dplyr::mutate(
+                Report_Name = report_name,
+                Report_Link = report_link,
+                Report_Released = report_released_date,
+                Valid_Links = TRUE,
+                Unknown_Error = FALSE
+              )
+
+            cli::cli_alert_success("{report_name} at {report_link} processed succesfully.")
+
+          }
+        )
+
 
       } else {
 
         final_report_details <- data.frame(
           Report_Name = report_name,
           Report_Link = report_link,
-          Valid = FALSE
+          Valid_Links = FALSE,
+          Unknown_Error = FALSE
         )
 
-        cli::cli_alert_danger("{report_name} at {report_link} does not have a submission and/or report page.")
+        cli::cli_alert_danger("{report_name} at {report_link} does not have a submission and/or report page equivalent.")
 
       }
 
--- R/valid_links.R
+++ R/valid_links.R
@@ -1,19 +1,95 @@
-valid_links <- function(links){
+valid_links <- function(report_link){
   UseMethod("valid_links")
 }
 
 #' @export
-valid_links.list <- function(links){
+valid_links.character <- function(report_link){
 
-  tryCatch(
 
-    error = function(cnd) isvalid <- FALSE,
-    {
-      lapply(links, function(link) rvest::read_html(link))
-      isvalid <- TRUE
-    }
-  )
 
-  isvalid
+  pages_suffix <- c(report_page = "/report/",
+                    submission_page = "/submissions/")
+  page_links <- lapply(pages_suffix, function(page_suffix) paste0(report_link, page_suffix))
+  valid_links <- test_links(page_links)
+
+  valid_links
+
+
 
 }
+
+# HELPER -----------------------------------------------------
+
+test_links <- function(links){
+
+  # This function should output a valid link list of 0, if none are found.
+
+  valid_links <- lapply(names(links), function(link_name){
+
+    link <-  links[[link_name]]
+
+    valid_link <- tryCatch(
+          error = function(cnd){
+            cli::cli_alert_danger("{link} failed. Testing alternatives.")
+            if (link_name == "report_page"){
+              other_report_page_suffixes <- c(strategy_page = "/strategy/")
+              valid_link <- test_alternative_pages(link = link,
+                                                   suffix_to_replace = "/report/",
+                                                   replacement_suffixes = other_report_page_suffixes)
+            } else if (link_name == "submission_page") {
+              other_submission_page_suffixes <- c()
+              # No known alternative names/links for submission pages.
+              valid_link <- test_alternative_pages(link = link,
+                                                   suffix_to_replace = "/submissions/",
+                                                   replacement_suffixes = other_submission_page_suffixes)
+            }
+
+            return(valid_link)
+
+            },
+          {
+            rvest::read_html(link)
+            valid_link <- link
+          }
+        )
+
+    valid_link
+  })
+
+  names(valid_links) <- names(links)
+  valid_links
+
+}
+
+test_alternative_pages <- function(link,
+                                   suffix_to_replace,
+                                   replacement_suffixes){
+
+  alternative_page_links <- lapply(replacement_suffixes, function(suffix) sub(suffix_to_replace, suffix, link))
+
+  valid_link_flags <- vapply(alternative_page_links, function(alternative_page_link){
+
+    tryCatch(
+      error = function(cnd){
+        cli::cli_alert_warning("{alternative_page_link} failed.")
+        valid_link <- FALSE
+      },
+      {
+        test <- rvest::read_html(alternative_page_link)
+        valid_link <- TRUE
+      }
+    )
+
+  }, logical(length(alternative_page_links)))
+
+  if (!any(valid_link_flags)){
+    cli::cli_alert_warning("Failed to read {link} alternatives.")
+    valid_link <- vector(mode = "character", length = 0)
+  } else {
+    valid_link <- alternative_page_links[[valid_link_flags]]
+    cli::cli_alert_success("{valid_link} alternative is successful.")
+  }
+
+  valid_link
+}
+
